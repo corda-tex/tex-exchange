1a4518c73371c8f0fdc06d46414d5d4bf8e9e001
7khedge <info@7khedge.com>
2019-11-15 12:05:58 +0000

    Moving Contract and State classes to contract module

--- tex-exchange-contract/src/main/kotlin/com/ccc/contract/ExecuteOrder.kt
+++ tex-exchange-contract/src/main/kotlin/com/ccc/contract/ExecuteOrder.kt
@@ -1,14 +1,16 @@
 package com.ccc.contract
 
+import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
 
+//@BelongsToContract(ExecuteOrderContract::class)
 data class ExecuteOrder(
     val owner: Party,
     val buyer: Party,
-    val orderReference: Order,
+    val orderLinearId: UniqueIdentifier,
     override val linearId: UniqueIdentifier = UniqueIdentifier()
 ) : LinearState {
     override val participants: List<AbstractParty> = listOf(buyer,owner)
