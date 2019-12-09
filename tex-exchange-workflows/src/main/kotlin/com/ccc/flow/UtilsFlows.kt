3a12a7b0e274c43ab936c3c11f10fdd43820971a
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2019-12-09 10:45:11 +0000

    Add forgotten modified UtilsFlows.kt

--- tex-exchange-workflows/src/main/kotlin/com/ccc/flow/UtilsFlows.kt
+++ tex-exchange-workflows/src/main/kotlin/com/ccc/flow/UtilsFlows.kt
@@ -1,2 +1,32 @@
 package com.ccc.flow
 
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
+import net.corda.finance.contracts.asset.Cash
+import net.corda.finance.contracts.utils.sumCash
+import net.corda.finance.flows.AbstractCashFlow
+
+object UtilsFlows {
+    class MergeCashFlow(progressTracker: ProgressTracker): AbstractCashFlow<Unit>(progressTracker) {
+        constructor(): this(tracker())
+        @Suspendable
+        override fun call() {
+            val me = ourIdentity
+            val cashStates = serviceHub.vaultService.queryBy(Cash.State::class.java).states
+            val txBuilder =
+                TransactionBuilder(notary = serviceHub.networkMapCache.notaryIdentities.first())
+            cashStates.forEach { txBuilder.addInputState(it) } // Add all the existing cash states.
+            try {
+                val cashSum = cashStates.map { it.state.data }.sumCash()
+                txBuilder.addOutputState(Cash.State(cashSum, me))
+                txBuilder.addCommand(Cash.Commands.Move(), me.owningKey)
+                val stx = serviceHub.signInitialTransaction(txBuilder)
+                finaliseTx(stx, emptySet(), "Unable to notarise issue").tx.outputsOfType(Cash.State::class.java).single()
+            } catch(e: UnsupportedOperationException) {
+                // thrown by sumCash()
+            }
+        }
+    }
+
+}
